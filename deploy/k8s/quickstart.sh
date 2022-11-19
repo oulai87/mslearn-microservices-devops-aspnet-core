beda164f6c0f3dfae3c665698e2405c01b82a9ea
Thomas O <oulaithomas05@gmail.com>
2022-11-19 16:54:19 -0600

    Changed script default location

--- deploy/k8s/quickstart.sh
+++ deploy/k8s/quickstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Change this for a different location
-defaultLocation="eastus"
+defaultLocation="centralus"
 # Change this for a different resource group name
 defaultRg="eshop-learn-rg"
 
